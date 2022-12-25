2c747455708d8ba3804d785f3f9ee3bfe73387b4
Tahsinul Haque Dhrubo <71776452+thDhrubo@users.noreply.github.com>
2022-12-25 23:00:13 +0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSumbit = async (e) => {
   
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://err0r404-ai-evjs.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type':'application/json'
